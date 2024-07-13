fda6b6bd854a3c14f4feec110de3f04fcc716d79
Vavadjan <vavadjan@gmail.com>
2024-07-13 23:01:48 +0300

    опять поправил

--- js/staticHeader.js
+++ js/staticHeader.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
+    const mainHeader = document.querySelector('.mainHeader');
     const staticHeader = document.querySelector('.staticHeader');
 
     if (window.scrollY > 177) {
-        header.style.transform = 'translateY(-100%)';
+        mainHeader.style.transform = 'translateY(-100%)';
         staticHeader.classList.add('visible');
     } else {
-        header.style.transform = 'translateY(0)';
+        mainHeader.style.transform = 'translateY(0)';
         staticHeader.classList.remove('visible');
     }
 });
