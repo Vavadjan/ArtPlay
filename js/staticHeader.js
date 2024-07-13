5eac03c8f23dd8e3743e08851e6af3b01d53ff2e
Vavadjan <vavadjan@gmail.com>
2024-07-13 22:06:57 +0300

    поправил js смены хедеров

--- js/staticHeader.js
+++ js/staticHeader.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function() {
-    const mainHeader = document.querySelector('.mainHeader');
+    const header = document.querySelector('header');
     const staticHeader = document.querySelector('.staticHeader');
 
     if (window.scrollY > 177) {
-        mainHeader.style.transform = 'translateY(-100%)';
+        header.style.transform = 'translateY(-100%)';
         staticHeader.classList.add('visible');
     } else {
-        mainHeader.style.transform = 'translateY(0)';
+        header.style.transform = 'translateY(0)';
         staticHeader.classList.remove('visible');
     }
 });
